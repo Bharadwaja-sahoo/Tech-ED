0320716584a59875b550540dfdc8262ed73c9234
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2025-01-27 17:31:57 +0530

    Initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+// setTimeout(function () {
+//   document.getElementById('loader').style.display = 'none';
+//   document.body.classList.remove('loading');
+// }, 2000);
+window.onload = function () {
+  AOS.init();
+  document.getElementById('loader').style.display = 'none';
+  document.body.classList.remove('loading');
+
+  AOS.refresh();
+};
+
+
+function isNumberKey(event) {
+  const charCode = event.which || event.keyCode;
+  // Allow only digits (48-57 in ASCII)
+  if (charCode < 48 || charCode > 57) {
+    return false;
+  }
+  return true;
+}
